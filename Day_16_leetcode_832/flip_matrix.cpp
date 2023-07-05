8a982fd9a3689ba410ee5827066b5caa00f2b66d
Radhika Gupta <118921713+Radhika26Gupta@users.noreply.github.com>
2023-07-05 16:47:11 +0530

    Add files via upload

---
+++ Day_16_leetcode_832/flip_matrix.cpp
@@ -0,0 +1,29 @@
+/*class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n = image.size();
+        for(int i=0;i<n;i++)
+        {
+            int start = 0 ;
+            int end = n-1;
+            while(start<end)
+            {
+                swap(image[i][start],image[i][end]);
+                start++;
+                end--;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(image[i][j]==0)
+                image[i][j] = 1;
+                else
+                image[i][j] = 0;
+            }
+        }
+        return image;
+        
+    }
+};*/
